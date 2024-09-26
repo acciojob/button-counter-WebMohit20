2bdcfd3b355c306ed6648f5860bfdbc01faa285d
WebMohit20 <2003mohitkhemani@gmail.com>
2024-09-26 03:29:47 +0000

    New changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
+import React ,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [x,setCount] = useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>{x}</p>
+        <button onClick={()=>setCount(x+1)}>Click</button>
     </div>
   )
 }
